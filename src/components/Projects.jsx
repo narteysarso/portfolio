de96304f98ccaf43e4e792953743a4ccf814a56a
narteysarso <narteysarso@gmail.com>
2022-09-30 08:30:16 +0000

    update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,21 +10,20 @@ export function Projects() {
       </Col>
       <Col span={24}>
         <Tabs tabPosition="left">
-          {categories.map((cat, idx) => (
-            <Tabs.TabPane tab={cat.toLocaleUpperCase()} key={idx}>
-              <Row gutter={[16,16]}>
+        <Tabs.TabPane tab={"All (17)"}>
+        <Row gutter={[16,16]}>
                 {projects.map((project, idx) => {
 
-                    if(project.category !== cat){
-                        return null
-                    }
                   return (<Col key={idx} xs={24} md={8} lg={6}>
-                    <Card
+                    <a href={project.liveurl}><Card
                       hoverable
+                      
                       cover={
                         <img
                           alt={project.title}
                           src={project.imageurl}
+                          title={`${project.title}`}
+                          
                         />
                       }
                       actions={[
@@ -52,7 +51,57 @@ export function Projects() {
                       <Space wrap size={"small"}>
                         {project.tags.map( (tag, idx) => <Tag color={"gold"} key={idx}>{tag}</Tag>)}
                       </Space>
-                    </Card>
+                    </Card></a>
+                  </Col>)
+                })}
+              </Row>
+        </Tabs.TabPane>
+          {categories.map((cat, idx) => (
+            <Tabs.TabPane tab={cat.toLocaleUpperCase()} key={idx}>
+              <Row gutter={[16,16]}>
+                {projects.map((project, idx) => {
+
+                    if(project.category !== cat){
+                        return null
+                    }
+                  return (<Col key={idx} xs={24} md={8} lg={6}>
+                    <a href={project.liveurl}><Card
+                      hoverable
+                      
+                      cover={
+                        <img
+                          alt={project.title}
+                          src={project.imageurl}
+                          title={`${project.title}`}
+                          
+                        />
+                      }
+                      actions={[
+                        <a
+                          href={project.liveurl}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <DesktopOutlined />
+                        </a>,
+                        <a
+                          href={project.github}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <GithubFilled />
+                        </a>,
+                      ]}
+                    >
+                      <Card.Meta
+                        title={project.title}
+                        description={project.description}
+                      />
+                      <Divider />
+                      <Space wrap size={"small"}>
+                        {project.tags.map( (tag, idx) => <Tag color={"gold"} key={idx}>{tag}</Tag>)}
+                      </Space>
+                    </Card></a>
                   </Col>)
                 })}
               </Row>
